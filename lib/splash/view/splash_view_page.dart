8bb547f03d8193e0f5613a0efdedce30f6ed2026
haruachm <85959639+haruachm@users.noreply.github.com>
2024-02-17 07:38:46 +0900

    refactor: Change initial setting

---
+++ lib/splash/view/splash_view_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashViewPage extends StatelessWidget {
+  static const url = "/splash";
+  const SplashViewPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
