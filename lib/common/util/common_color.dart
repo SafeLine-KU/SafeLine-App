8687c9d98271f1e1c46495e007e18b36261fd873
haruachm <85959639+haruachm@users.noreply.github.com>
2024-02-19 02:30:28 +0900

    feaat: Create Color component

--- lib/common/util/common_color.dart
+++ lib/common/util/common_color.dart
@@ -3,7 +3,28 @@ import 'package:flutter/material.dart';
 class ColorTheme {
   static const Color primaryColor = Color(0xFFFA4549);
 
-//System Color
+  //Icon Color
+  static const Color iconActiveColor = Color(0xFF0AC2A1);
+  static const Color iconInActiveColor = Color.fromARGB(255, 142, 140, 134);
+
+  //Grey Color
+  static const Color grey_F7F7F7 = Color(0xFFF7F7F7);
+  static const Color grey_ECECEC = Color(0xFFECECEC);
+  static const Color grey_DFDFDF = Color(0xFFDFDFDF);
+  static const Color grey_C1C1C1 = Color(0xFFC1C1C1);
+  static const Color grey_8B8B8B = Color(0xFF8B8B8B);
+  static const Color grey_555555 = Color(0xFF555555);
+  static const Color grey_3D3D3D = Color(0xFF3D3D3D);
+  static const Color grey_333333 = Color(0xFF333333);
+  static const Color grey_242424 = Color(0xFF242424);
+  static const Color grey_171717 = Color(0xFF171717);
+  static const Color grey_878787 = Color(0xFF878787);
+  static const Color grey_D6D6D6 = Color(0xFFD6D6D6);
+  static const Color grey_E8E8E8 = Color(0xFFE8E8E8);
+  static const Color grey_EBEBEB = Color(0xFFEBEBEB);
+  static const Color grey_B0B0B0 = Color(0xFFB0B0B0);
+
+  //System Color
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
   static const Color errorColor = Color(0xFFF80003);
