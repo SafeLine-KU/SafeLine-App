22e093ec8bfb2268a50508565657ead69a724af4
haruachm <85959639+haruachm@users.noreply.github.com>
2024-02-25 20:21:39 +0900

    fix: Fix model name

---
+++ lib/common/global_widget/marker.dart
@@ -0,0 +1,17 @@
+class MarkerInfo {
+  final String name;
+  final String address;
+  final String area;
+  final double distance;
+  final int groundFloor;
+  final int basementFloor;
+
+  MarkerInfo({
+    required this.name,
+    required this.address,
+    required this.area,
+    required this.distance,
+    required this.groundFloor,
+    required this.basementFloor,
+  });
+}
